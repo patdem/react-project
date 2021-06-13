2337cedffaaa595b1de97097b82a0ab38ce9a89f
patdem <patrycjadembinska60@gmail.com>
2021-06-13 13:33:20 +0200

    update cart page

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,7 +1,7 @@
 export const Cart = () => {
   return (
-    <div className="Welcome bg-info bg-gradient">
-      <h1>Cart Page!</h1>
+    <div className="bg-info">
+      <h1>Your cart</h1>
     </div>
   );
 }
