c4224db22ae9e46719ee2db0328189e8fe8f5879
patdem <patrycjadembinska60@gmail.com>
2021-06-13 13:32:52 +0200

    update products page

--- src/components/ProductsPage/Product.jsx
+++ src/components/ProductsPage/Product.jsx
@@ -1,12 +1,23 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import React from "react";
+import product from "../../img/product.svg";
+
 const Product = (props) => {
   return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{props.category}</h6>
-        <p className="card-text">{props.description}</p>
-      </div>
-    </div>
+    <Card style={{ width: '20rem', marginRight: "auto", marginLeft: "auto", marginTop: 10, marginBottom: 10,  padding:20 }}>
+      <Card.Img variant="top" src={product} />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>
+          {props.description}
+        </Card.Text>
+        <Card.Text>
+          <small className="text-muted">{props.category}</small>
+        </Card.Text>
+        <Button variant="primary">Add to cart</Button>
+      </Card.Body>
+    </Card>
   );
 };
 
